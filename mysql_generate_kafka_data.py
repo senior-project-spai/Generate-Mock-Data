3e0f8945feaf87b53c6045e3638186ddd6865904
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-10-18 02:03:57 +0700

    Create mysql_generate_kafka_data.py

---
+++ mysql_generate_kafka_data.py
@@ -0,0 +1,24 @@
+import json
+import time
+import random
+from kafka import KafkaProducer
+
+KAFKA_HOST = os.environ['KAFKA_HOST']
+KAFKA_PORT = os.environ['KAFKA_PORT']
+KAFKA_TOPIC_OUTPUT = os.environ['KAFKA_TOPIC_OUTPUT']
+
+producer = KafkaProducer(bootstrap_servers=['{}:{}'.format(KAFKA_HOST, KAFKA_PORT)],value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+while True:
+    mock_data = {
+        'time': time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()),
+        'Gender':random.choice(['Male','Female']),
+        'Race':random.choice(['Asian','White']),
+        'position_top':random.randint(0,500),
+        'position_left':random.randint(0,500),
+        'position_right':random.randint(0,500)+random.randint(0,500),
+        'position_bottom':random.randint(0,500)+random.randint(0,500),
+    }
+    print(mock_data)
+    producer.send('KAFKA_TOPIC_OUTPUT', mock_data)
+    time.sleep(random.uniform(0,30))
